e26a658f03ce1774e1555d2896f0ec1676b25373
DESKTOP-RR1GA0M\robot <robot.stopari@skaut.cz>
2020-12-10 17:19:28 +0100

    Add content in Kucharka, add Mapa and fix small font-size on small displays

--- elements/page.php
+++ elements/page.php
@@ -6,8 +6,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="/styles/styles.css">
+    <link rel="stylesheet" href="/styles/inspirace.css">
     <title>Akce</title>
-    <link rel="icon" href="images/main/LogoFullTr.png">
+    <link rel="icon" href="/images/main/LogoFullTr.png">
     <link href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@400;700&display=swap" rel="stylesheet">
 </head>
 
--- elements/pages.php
+++ elements/pages.php
@@ -3,55 +3,83 @@ $pages = [
     [
         "heading" => "Naše kronika",
         "url" => "/inspiruj-se/nase-kronika",
-        "content" => "Toto jest nase kronika...",
+        "content" => "<p>Zde naleznete naši kroniku - všechny zápisy a zážitky z našich akcí a výprav. Můžete se zde inspirovat, nebo si jen pročíst, co lze všechno zažít v družinovém životě.</p><p>Tuto stránku pro vás ještě připravujeme.</p>",
         "color" => "green",
     ],
     [
         "heading" => "Náš rituálník",
         "url" => "/inspiruj-se/nas-ritualnik",
-        "content" => "",
+        "content" => "<p>Zde se brzy dozvíte vše o našich družinových rituálech, zvycích a tradicích.</p>",
         "color" => "blue",
     ],
     [
         "heading" => "Naše kuchařka",
         "url" => "/inspiruj-se/nase-kucharka",
-        "content" => "",
+        "content" => "<p>Nemáte nápady na to, co vařit na vašich akcích? Jsou vaše obědy stále stejné?</p><p>Podívejte se do naši kuchařky! Všechna tato jídla jsme několikrát otestovali a vyladili do posledních detailů a jsou tak jednoduchá, že je zvládnete uvařit všichni.
+        <h2>Spinosaurská cibulačka</h2>
+        <p>Potřeby: 3 bílé cibule, 3 červené cibule, mouka, cukr, bageta, sýr, sůl, pepř, bobkový list, máslo</p>
+        <p>Doba přípravy: 60 minut</p>
+        <p>Porce: 5</p>
+        <p>Postup: Bílé cibule nakrájíme na kolečka a na másle je osmažíme do změknutí a zvláčnění. Do hrnce přidáme panák vody (nebo bílého vína), polévkovou lžíci cukru a 2 bobkové listy. Dusíme pod pokličkou asi pět minut. <br>
+        V druhém hrnci smažíme na másle červenou cibuli nakrájenou na kolečka, po změknutí ji za smažení zasypeme moukou a vytvoříme hustou hmotu. Bílou cibuli též zasypeme moukou a tyto dvě husté hmoty sesypeme do většího hrnce a zalijeme horkou vodou. Vodu osolíme a opepříme. <br>
+        Nakrájenou bagetu zapečeme v troubě s nastrouhaným sýrem a kousky másla, okořeníme ji dle chuti do křupava. Podáváme cibulačku se strouhaným sýrem a křupavou teplou zapečenou bagetkou.</p>
+        
+        <h2>Spinosaurské ratatouille</h2>
+        <p>Potřeby: 1 lilek, 2 cukety, 2 rajčata, 2 červené papriky, 1 plechovka loupaných rajčat, 4 stroužky česneku, olivový olej, tymián, rozmarýn, 2 cibule, pepř, sůl, bílé pečivo</p>
+        <p>Doba přípravy: 90 minut</p>
+        <p>Porce: 5</p>
+        <p>Postup: Nakrájíme lilek, cibule a rajčata na kostičky, papriky na proužky, česnek na jemné plátky. Cukety podélně rozkrojíme a nakrájíme na půlcentimetrové kostky.<br>
+        V hrnci rozpálíme olivový olej a na něm dozlatova smažíme zeleninu v pořadí: lilek, papriky, rajčata, cibule, česnek a cuketa.<br>
+        Nyní zalijeme rajčaty z plechovky, osolíme, opepříme, promícháme směs a dusíme ji poloodkrytou pod pokličkou. Počkáme, než se odpaří většina šťávy z rajčat.<br>
+        Přidáme dvě lžíce olivového oleje, tymián a rozmarýn.<br>
+        Podáváme nejlépe s čerstvým bílým pečivem.</p>
+
+        <h2>Lečo po Spinosaursku</h2>
+        <p>Potřeby: 3 cibule, 6 paprik, 5 rajčat, sůl, pepř, provensálské byliny, tymián, máslo, 4 klobásy (dle velikosti), 3 stroužky česneku, pečivo</p>
+        <p>Doba přípravy: 45 minut</p>
+        <p>Porce: 5</p>
+        <p>Na másle osmažíme na kostičky nakrájenou cibulku do změknutí, přidáme natřený česnek. Po osmažení cibule přidáme na kolečka nakrájené klobásy a smažíme je do vypuštění tuku.<br>
+        Přidáme na kostičky nakrájené papriky a půl panáku vody, dusíme do změknutí paprik.<br>
+        Přidáme na kostičky nakrájená rajčata se šťávou a v ní vaříme do odpaření většiny vody. Osolíme a opepříme, přidáme koření dle chuti.<br>
+        Lečo se skvěle hodí na puťáky, nejlépe na první večeře, kdy je zelenina a pečivo ještě čerstvé.</p>
+        ",
         "color" => "brown",
     ],
     [
-        "heading" => "Výzvy",
+        "heading" => "Výzva",
         "url" => "/inspiruj-se/vyzvy",
-        "content" => "",
+        "content" => "<p>Nevíte co dělat? Chybí vám ispirace? Potřebujete pořádnou výzvu nebo se jen chcete překonat?</p><p>Na této stránce bude vždy jedna výzva pro vaši družinu, pro jejíž splnění musíte spojit všechny síly!</p><h2>Pokřik!</h2><p>Natočte nebo nahrajte se svou dužinou váš pokřik tím nejoriginálnějším způsobem, který existuje. Pošlete nám svou epickou nahrávku na Slack!</p>",
         "color" => "brown",
     ],
     [
         "heading" => "Osobní rozvoj",
         "url" => "/inspiruj-se/osobni-rozvoj",
-        "content" => "",
+        "content" => "<p>Zde se můžete dočíst o našem osobním rozvoji. Každý z nás se nějak posouvá v určitch oblastech a máme stanovené naše rozvojové cíle.</p><p>Tuto stránku pro vás připravujeme.</p>",
         "color" => "green",
     ],
     [
         "heading" => "Odborky",
         "url" => "/inspiruj-se/odborky",
-        "content" => "",
+        "content" => "<p>Každý Spinosaurus si plní jednu odborku. O našem postupu v plnění se můžete dočíst zde.</p><p>Tuto stránku pro vás připravujeme.</p>",
         "color" => "blue",
     ],
     [
         "heading" => "Mapa",
-        "url" => "/inspiruj-se/mapa",
-        "content" => "",
+        "url" => "https://www.google.com/maps/d/u/0/edit?mid=1GE20-eIR3nGmsU_Ucel7hn0lCuj4ftAm&usp=sharing",
+        "content" => "<p>Byli jsme již na hodně zajímavých místech. Na této mapě uvidíte všechna místa, kde proběhly Spinosaurské výpravy či schůzky.</p><p>Mapu pro vás připravujeme.</p>",
         "color" => "green",
+        "target" => "target=\"_blank\"",
     ],
     [
         "heading" => "Kurzy",
         "url" => "/inspiruj-se/kurzy",
-        "content" => "",
+        "content" => "<p>Každý z nás byl na nějakých kurzech. Z kurzů máme mnohé prožitky a super zkušenosti. Zde si můžete přečíst, kde kdo z nás byl a co si o kurzech myslí.</p><p>Stránku pro vás připravujeme.</p>",
         "color" => "brown",
     ],
     [
         "heading" => "Videotéka",
         "url" => "/inspiruj-se/videoteka",
-        "content" => "",
+        "content" => "<p>Nezaostáváme v moderních technologiích a tak jsme natočili mnohá zajímavá i vtipná videa. Všechna videa naleznete na této stránce.</p><p>Videotéku pro vás připravujeme.</p>",
         "color" => "blue",
     ],
 ];
