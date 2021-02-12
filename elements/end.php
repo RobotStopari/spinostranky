b929e002429aaab63539313d2e1f3a9d2b334b2c
Jakub Procházka - PC <robot.stopari@skaut.cz>
2021-02-12 08:54:09 +0100

    Remove useless php from elements

--- elements/end.php
+++ elements/end.php
@@ -1,8 +1,4 @@
-<?php
-
-echo '
         </main>
     </div>
 </body>
 </html>
-';
--- elements/navigation.php
+++ elements/navigation.php
@@ -1,13 +1,13 @@
-    <nav class="head">
-        <div class="head-float">
-            <a href="/"><img src="/images/main/Logo.png" alt="logo" class="logo"></a>
-            <ul class="navbar">
-                <li class="navbar-item"><a href="/" class="navbar-link">O nás</a></li>
-                <li class="navbar-item"><a href="/clenove" class="navbar-link">Členové</a></li>
-                <li class="navbar-item"><a href="/inspiruj-se" class="navbar-link">Inspirace</a></li>
-                <li class="navbar-item"><a href="/nase-fotky" class="navbar-link">Fotky</a></li>
-            </ul>
-        </div>
-    </nav>
-    <div class="page">
-        <main class="page-float">
+<nav class="head">
+    <div class="head-float">
+        <a href="/"><img src="/images/main/Logo.png" alt="logo" class="logo"></a>
+        <ul class="navbar">
+            <li class="navbar-item"><a href="/" class="navbar-link">O nás</a></li>
+            <li class="navbar-item"><a href="/clenove" class="navbar-link">Členové</a></li>
+            <li class="navbar-item"><a href="/inspiruj-se" class="navbar-link">Inspirace</a></li>
+            <li class="navbar-item"><a href="/nase-fotky" class="navbar-link">Fotky</a></li>
+        </ul>
+    </div>
+</nav>
+<div class="page">
+    <main class="page-float">
