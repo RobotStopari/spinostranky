0af6d5422ce9b78a03a079001733160f76fc0b30
Jakub Procházka - PC <robot.stopari@skaut.cz>
2021-03-28 20:38:45 +0200

    Add external page icon to the same line as heading in photogallery

--- elements/gallery.php
+++ elements/gallery.php
@@ -2,6 +2,6 @@
     <a href="<?php echo $link ?>" target="_blank">
         <img class="window-img" src="images/<?php echo $img ?>" alt="<?php echo $title ?>" style="<?php echo $style ?>">    
     </a>
-    <img src="../images/main/externalBlack.png" class="external-icon-photo">
-    <h2 class="window-heading"><?php echo $title ?></h2>
+    <h2 class="window-heading"><?php echo $title ?>
+    <img src="../images/main/externalBlack.png" class="external-icon-photo"></h2>
 </div>
